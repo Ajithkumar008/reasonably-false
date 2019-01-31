0cf8735f902cf43a407b1812ba63bac721b6dad3
Ajithkumar008 <46957944+Ajithkumar008@users.noreply.github.com>
2019-01-31 11:07:35 +0000

    Create odd number range.py

---
+++ odd number range.py
@@ -0,0 +1,7 @@
+N,Q=map(int,input("enter two values").split())
+if(N<=10000 and Q<=10000):
+    for i in range(N,Q,1):
+        if(i%2==1):
+            print (i)
+else:
+    print("range  is only between 10000")
